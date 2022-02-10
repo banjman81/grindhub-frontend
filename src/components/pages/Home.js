0ee96a8c6fdc71e467eef59ce2a4d320e4557b91
banjman81 <70820423+banjman81@users.noreply.github.com>
2022-02-10 15:05:18 -0600

    reducer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
 
 function Home() {
+    const [planets, setPlanets] = useState([])
+
+    
+    async function initialLoad(){
+        try{
+            let payload = await axios.get('https://swapi.dev/api/planets')
+            setPlanets(payload.data.results)
+        }catch(err){
+            console.log(err.message)
+        }
+    }
+
+    useEffect(() => {
+        initialLoad()
+    }, [])
+
+    
     return (
         <div>
             <h1>Home</h1>
             <Link to='/em'><h3>Look for works</h3></Link>
+            <ul style={{background: '', minHeight: '50px'}}>
+                {planets?.map((planet) => (
+                <li key={planet.name}>{planet.name}</li>
+                ))}
+            </ul>
+            
         </div>);
 }
 
--- src/components/pages/HomeEm.js
+++ src/components/pages/HomeEm.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
 import { Link } from 'react-router-dom';
 
+const initialState = {count : 0, text: 'This is my initial text'}
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'increment':
+            return {count: state.count + 1}
+        case 'decrement':
+            return {count: state.count - 1}
+        case 'zero':
+            return {count: 0}
+        case 'double' :
+            return {count: state.count * 2}
+        case 'half' :
+            return {count: state.count / 2}
+        case 'log' :
+            console.log(eval(action.message))
+            return {count: action.message}
+        default:
+            throw new Error()
+    }
+}
+
 function HomeEm() {
+    const [myValue, dispatch] = useReducer(reducer, initialState)
+    const [input, setInput] = useState("")
     return (
         <div>
             <h1>Home Em</h1>
             <Link to='/'><h3>Look for jobs</h3></Link>
+            {myValue.count}
+            <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+            <button onClick={() => dispatch({type: 'increment'})}>+</button>
+            <button onClick={() => dispatch({type: 'half'})}>half</button>
+            <button onClick={() => dispatch({type: 'double'})}>double</button>
+            <button onClick={() => dispatch({type: 'zero'})}>zero</button>
+            <input type="number" onChange={(e) => setInput(e.target.value) } />
+            <button onClick={() => dispatch({type: 'log', message: input})}>log</button>
         </div>);
 }
 
